db1f478d292d3df88901941f1899efa91bfffe40
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-01-02 19:59:58 +0100

    create plugin

---
+++ src/io/koara/Activator.java
@@ -0,0 +1,35 @@
+package io.koara;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "io.koara.koara"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	public Activator() {
+	    System.out.println("ACTIVATE KOARA");
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
