4d14474646cfe06eb5ae5f7dc104f275e872fd9d
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-01-03 18:10:38 +0100

    add custom editor

---
+++ src/io/koara/editors/KoaraEditor.java
@@ -0,0 +1,14 @@
+package io.koara.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class KoaraEditor extends TextEditor {
+
+	public KoaraEditor() {
+		super();
+	}
+	public void dispose() {
+		super.dispose();
+	}
+
+}
