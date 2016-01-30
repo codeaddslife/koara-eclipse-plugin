1cdeaa61b7c71e9997f398c715fdc8760b45e62d
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-01-30 15:39:05 +0100

    remove syntax highlighting

--- plugin/src/io/koara/eclipse/editors/KoaraEditor.java
+++ plugin/src/io/koara/eclipse/editors/KoaraEditor.java
@@ -4,18 +4,7 @@ import org.eclipse.ui.editors.text.TextEditor;
 
 public class KoaraEditor extends TextEditor {
 
-	private ColorManager colorManager;
-
 	public KoaraEditor() {
-		super();
-		colorManager = new ColorManager();
-		setSourceViewerConfiguration(new KoaraConfiguration(colorManager));
-		setDocumentProvider(new KoaraDocumentProvider());
-	}
-	
-	public void dispose() {
-		colorManager.dispose();
-		super.dispose();
-	}
+	}	
 
 }
